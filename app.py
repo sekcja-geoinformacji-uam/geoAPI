c2a368fceae2f22c3be3c81e42b151072f06c90c
Tomek Matuszek <tom.mateuszek@gmail.com>
2022-11-13 11:39:18 +0100

    endpoint tomka

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/tomek")
+def wodzionka():
+    text = "<p>Mmmm. Wodzionka, suchy chlyb i wody szklonka.<br>Mmmm. Wodzionka, to nojlepszo z wszystkich ślonskich zup.<br> Mmmm. Wodzionka, jak jom zrobi moja żonka.<br> Mmmm. Wodzionka, to jes łósmy świata cud.</p>"
+    return text
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
