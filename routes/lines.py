328837c21f3ddc57140586511279f94c9e1911f2
Tomek Matuszek <tom.mateuszek@gmail.com>
2023-07-02 17:16:48 +0200

    interpolate routing

--- routes/lines.py
+++ routes/lines.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request
 import json as jsn
 import numpy as np
 import geopandas as gpd
-from shapely.geometry import Point
+from shapely.geometry import Point, MultiPoint
 from shapely.coordinates import get_coordinates
 from pyproj.exceptions import CRSError
 from utils.get_UTM_zone import get_UTM_zone
@@ -43,3 +43,25 @@ def vertices():
     points = [Point(x, y) for x, y in coords.tolist()]
     points = gpd.GeoDataFrame({'line': idx.tolist(), 'sequence': sequences, 'geometry': points})
     return points.to_json()
+
+@lines_bp.post('/interpolate')
+def interpolate():
+    json = jsn.dumps(request.json)
+    lines:gpd.GeoDataFrame = gpd.read_file(json, driver='GeoJSON')
+    spacing = request.args.get('spacing', default=None)
+    n_points = request.args.get('n_points', default=None)
+    spacing = float(spacing) if spacing is not None else spacing
+    n_points = int(n_points) if n_points is not None else n_points
+
+    pointsResult = lines.copy()
+    for i, line in lines.iterrows():
+        if spacing is not None:
+            distances = np.arange(0, line.geometry.length, spacing)
+            pointsList = [line.geometry.interpolate(d) for d in distances]
+            pointsResult.loc[i, 'geometry'] = MultiPoint(pointsList)
+        elif n_points is not None:
+            distances = np.linspace(0, line.geometry.length, n_points)
+            pointsList = [line.geometry.interpolate(d) for d in distances]
+            pointsResult.loc[i, 'geometry'] = MultiPoint(pointsList)
+    
+    return pointsResult.to_json()
